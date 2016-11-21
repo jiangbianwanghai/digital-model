665ea4f115afd746b5a55d3c1b187be51ca1483e
jiangbianwanghai <webmaster@jiangbianwanghai.com>
2016-11-21 16:03:54 +0800

    调整单元测试

--- tests/testsTestUnitTest.php
+++ tests/testsTestUnitTest.php
@@ -13,10 +13,5 @@
              'works',
              'This is OK'
          );
-
-         $this->assertEquals('works',
-             'works1',
-             'This will fail'
-         );
      }
  }
